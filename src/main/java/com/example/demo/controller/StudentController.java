7f6f1b1d5d62155fd163e6e7864a78fec65c5051
Rajyavardhan36 <rabbireddi@red-shift.us>
2025-06-12 13:08:44 +0530

    Updated controller msg.

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -21,7 +21,7 @@ public class StudentController {
     @GetMapping("test")
     public String test()
     {
-        return "Hello Student Controller API";
+        return "Hello This is Student Controller REST API";
     }
     @PostMapping("add")
     public void postStudent(@RequestBody Student s)
